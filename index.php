f44598f39c235c77d496618a0962b8d9f96f3a2b
Mike Mazdra <mazdram@health.missouri.edu>
2015-10-08 10:51:43 -0500

    Added several ghost button variations to _buttons.scss

--- index.php
+++ index.php
@@ -7,7 +7,15 @@
 </head>
 
 <body>
-<a class="ghost-button" href="#">Ghost button text</a>
+<a class="ghost-button" href="#">.ghost-button</a>
+
+<a class="ghost-button-rounded" href="#">.ghost-button-rounded</a>
+
+<a class="ghost-button-thick" href="#">.ghost-button-thick</a>
+
+<a class="ghost-button-full-color" href="#">.ghost-button-full-color</a>
+
+<a class="ghost-button-size-transition" href="#">.ghost-button-size-transition</a>
 </body>
 
 </html>
