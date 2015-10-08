e9abe234061645a581adaf9321dc2d7ed5e153e7
Mike Mazdra <mazdram@health.missouri.edu>
2015-10-08 11:54:19 -0500

    Added a separate page for the Kitchen Sink, and proxima nova base typography

--- index.php
+++ index.php
@@ -5,17 +5,17 @@
 <title>Basic Kitchen Sink</title>
 <link href="css/main.css" media="all" rel="stylesheet"/>
 </head>
-
 <body>
-<a class="ghost-button" href="#">.ghost-button</a>
 
-<a class="ghost-button-rounded" href="#">.ghost-button-rounded</a>
 
-<a class="ghost-button-thick" href="#">.ghost-button-thick</a>
 
-<a class="ghost-button-full-color" href="#">.ghost-button-full-color</a>
+<p>The MU School of Medicine has formed a unique partnership with CoxHealth and Mercy health systems in Springfield that will alleviate Missouri’s critical physician shortage by training more doctors to provide patient-centered care for the people of the state and beyond.</p>
+<p>Beginning in 2016 with the opening of a second MU clinical campus in Springfield and in 2017 with the opening of the Patient-Centered Care Learning Center in Columbia, the University of Missouri will be expanding its medical school class to provide more than 300 additional physicians for Missouri.</p>
+<p>The MU School of Medicine model explicitly focuses students’ attention on the people they are preparing to serve, and students’ clinically-based education will draw on the patient-centered care provided by CoxHealth, Mercy and University of Missouri Health Care.</p>
+<p>MU’s unique partnership will serve as a model for other areas of the state — and nation — that face shortages of health care professionals.</p>
 
-<a class="ghost-button-size-transition" href="#">.ghost-button-size-transition</a>
+
+
+
 </body>
-
 </html>
